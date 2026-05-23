3d5826ada0e8f67722629dcbe5649a14dce42359
TheGr3atJosh <90441217+TheGr3atJosh@users.noreply.github.com>
2026-05-23 18:01:09 +0200

    feat(30-01): create TK-BOF/tkerror.h with TkErrorMessage helper
    - Static inline TkErrorMessage wrapping KERNEL32$FormatMessageA (flags 0x1300)
    - On success: copies FormatMessage text, strips trailing CR/LF/space, frees sysBuf
    - On failure: fallback via MSVCRT$_snprintf with 'error %lu' format
    - Header guard _TKERROR_H_, includes bofdefs.h (relative to TK-BOF/)
    - No FsErrorMessage/FsNormalizeSlashes/base.c references

---
+++ TK-BOF/tkerror.h
@@ -0,0 +1,45 @@
+#ifndef _TKERROR_H_
+#define _TKERROR_H_
+
+#include <windows.h>
+#include "bofdefs.h"
+
+static inline void TkErrorMessage(DWORD dwError, char *buf, int bufSize)
+{
+	char  *sysBuf = NULL;
+	DWORD  ret    = 0;
+	int    i      = 0;
+
+	ret = KERNEL32$FormatMessageA(
+		0x1300,       // FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS
+		NULL,
+		dwError,
+		0,
+		(LPSTR) &sysBuf,
+		0,
+		NULL
+	);
+
+	if (ret > 0 && sysBuf != NULL)
+	{
+		for (i = 0; i < bufSize - 1 && sysBuf[i] != '\0'; i++)
+		{
+			buf[i] = sysBuf[i];
+		}
+		buf[i] = '\0';
+
+		while (i > 0 && (buf[i-1] == '\r' || buf[i-1] == '\n' || buf[i-1] == ' '))
+		{
+			i--;
+			buf[i] = '\0';
+		}
+
+		KERNEL32$LocalFree((HLOCAL) sysBuf);
+	}
+	else
+	{
+		MSVCRT$_snprintf(buf, bufSize, "error %lu", dwError);
+	}
+}
+
+#endif // _TKERROR_H_
